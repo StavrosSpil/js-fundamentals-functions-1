1cdedebd941d0c0f199a5069792d7ad84aa8bf92
Stavros <spiliopoulos.stavros@hotmail.com>
2023-04-11 19:39:24 +0300

    Stavros

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function increment(num) {
+  return num + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +21,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +35,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function sayHello(name) {
+  return 'Hi, ' + name.charAt(0).toUpperCase() + name.slice(1) + ' :)'
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +50,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function howManyStrings(array) {
+  let total = 0
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (typeof array[i] === 'string') {
+      total += 1
+    }
+  }
+  return total
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +70,27 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addProperty(object) {
+  if (object.edward === undefined) {
+    object.edward = 'amazing'
+  }
+  return object
+  // let array = []
+  // array = Object.keys(object)
+  // console.log(array)
+  // if (array.length === 0) {
+  //   object.edward = 'amazing'
+  // }
+  // if (object.empty(Object.hasOwn(object, 'edward') === false)) {
+  //   object.edward = 'amazing'
+  // }
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalizeFirstLetter, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: sayHello, // etc
+  d: howManyStrings,
+  e: addProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function twoNumbers(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function upperCase(string, number) {
+  let exclamationMarks = ''
+  const upperCaseString = string.toUpperCase()
+  for (let i = 1; i <= number; i++) {
+    exclamationMarks += '!'
+  }
+  return upperCaseString + exclamationMarks
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -36,7 +51,7 @@
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: twoNumbers, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: upperCase, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
   c: undefined // etc
 }
